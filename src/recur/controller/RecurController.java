7216d17c6a587a2c7d00ffeed4e4e2a11d5c6f90
Maddie-K <68201060+Maddie-K@users.noreply.github.com>
2021-03-10 12:15:45 -0700

    imported the popup, added cod in the start and created the fibRecursive

--- src/recur/controller/RecurController.java
+++ src/recur/controller/RecurController.java
@@ -1,9 +1,38 @@
 package recur.controller;
 
+import recur.view.Popup;
+
 public class RecurController
 {
+	private Popup view;
+	
+	public RecurController()
+	{
+		this.view = new Popup();
+	}
 	public void start()
 	{
+		String text = view.askQuestion("What should we try or blank to exit?");
 		
+		while (!text.equals(""))
+		{
+			int number = Integer.parseInt(text);
+			int result = fibRecursive(number);
+			view.showMessage("The" + number + "th fibonacci is:" + result);
+			
+			text = view.askQuestion("type in the next value or blank to end.");
+		}
+	}
+	
+	private int fibRecursive(int input)
+	{
+		if (input == 0 || input == 1)
+		{
+			return 1;
+		}
+		else
+		{
+			return fibRecursive(input - 1) + fibRecursive(input - 2);
+		}
 	}
 }
